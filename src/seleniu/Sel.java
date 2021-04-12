7374b66499172d03ec434bd6cee7fdf99c7d6e41
prakash2106 <prakash.aero.engineer@gmail.com>
2021-04-12 22:36:53 +0530

    finally

---
+++ src/seleniu/Sel.java
@@ -0,0 +1,16 @@
+package seleniu;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Sel {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.gecko.driver","C:\\Users\\sony\\eclipse-workspace\\PRAKASH\\seleniu\\driver\\geckodriver.exe");
+	WebDriver dr= new FirefoxDriver();
+dr.get("http://greenstech.in/selenium-course-content.html");
+	}
+
+}
+
+
+
